091ebb17bc9ce2a38b349413008f943509ed94d7
ravindu-shashika <ravindushashika98@gmail.com>
2026-07-09 21:46:00 +0530

    fix isses

--- pos-api-new/tests/Feature/ExampleTest.php
+++ pos-api-new/tests/Feature/ExampleTest.php
@@ -8,11 +8,11 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Laravel registers a health route at /up (see bootstrap/app.php).
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/up');
 
         $response->assertStatus(200);
     }
